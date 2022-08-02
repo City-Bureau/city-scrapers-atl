29cfdd27326e9c90d5fc127eafaca4c486d9fe95
James Turk <consulting@jamesturk.net>
2022-08-01 21:29:40 -0500

    move all ATL council spiders into same file

--- city_scrapers/spiders/atl_city_council.py
+++ city_scrapers/spiders/atl_city_council.py
@@ -84,3 +84,45 @@ class AtlCityCouncilSpider(CityScrapersSpider):
     def _parse_source(self, response):
         """Parse or generate source."""
         return response.url
+
+
+class AtlCityCouncilFinSpider(AtlCityCouncilSpider):
+    name = "atl_city_council_fin"
+    board_name = "Finance/Executive Committee"
+    agency = "Atlanta City Council: {}".format(board_name)
+
+
+class AtlCityCouncilUtilSpider(AtlCityCouncilSpider):
+    name = "atl_city_council_utilities"
+    board_name = "City Utilities Committee"
+    agency = "Atlanta City Council: {}".format(board_name)
+
+
+class AtlCityCouncilCOCSpider(AtlCityCouncilSpider):
+    name = "atl_city_council_coc"
+    board_name = "Committee on Council"
+    agency = "Atlanta City Council: {}".format(board_name)
+
+
+class AtlCityCouncilCDHSpider(AtlCityCouncilSpider):
+    name = "atl_city_council_community"
+    board_name = "Community Development/Human Services Committee"
+    agency = "Atlanta City Council: {}".format(board_name)
+
+
+class AtlCityCouncilSafetySpider(AtlCityCouncilSpider):
+    name = "atl_city_council_safety"
+    board_name = "Public Safety & Legal Administration Committee"
+    agency = "Atlanta City Council: {}".format(board_name)
+
+
+class AtlCityCouncilTransportationSpider(AtlCityCouncilSpider):
+    name = "atl_city_council_transportation"
+    board_name = "Transportation Committee"
+    agency = "Atlanta City Council: {}".format(board_name)
+
+
+class AtlCityCouncilZoningSpider(AtlCityCouncilSpider):
+    name = "atl_city_council_zoning"
+    board_name = "Zoning Committee"
+    agency = "Atlanta City Council: {}".format(board_name)
--- city_scrapers/spiders/atl_city_council_fin.py
+++
@@ -1,7 +0,0 @@
-from .atl_city_council import AtlCityCouncilSpider
-
-
-class AtlCityCouncilFinSpider(AtlCityCouncilSpider):
-    name = "atl_city_council_fin"
-    board_name = "Finance/Executive Committee"
-    agency = "Atlanta City Council: {}".format(board_name)

3d15fb17214d4d70eda03d70c142502e70f83d65
James Turk <consulting@jamesturk.net>
2022-08-01 21:37:36 -0500

    add tests for each ATL city council subscraper

--- tests/test_atl_city_council.py
+++ tests/test_atl_city_council.py
@@ -1,10 +1,20 @@
 from datetime import datetime
 from os.path import dirname, join
 
+import pytest
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
-from city_scrapers.spiders.atl_city_council import AtlCityCouncilSpider
+from city_scrapers.spiders.atl_city_council import (
+    AtlCityCouncilCDHSpider,
+    AtlCityCouncilCOCSpider,
+    AtlCityCouncilFinSpider,
+    AtlCityCouncilSafetySpider,
+    AtlCityCouncilSpider,
+    AtlCityCouncilTransportationSpider,
+    AtlCityCouncilUtilSpider,
+    AtlCityCouncilZoningSpider,
+)
 
 test_response = file_response(
     join(dirname(__file__), "files", "atl_city_council.html"),
@@ -46,3 +56,22 @@ def test_links():
             "title": "Jan 3, 2022 1:00 PM ",
         }
     ]
+
+
+@pytest.mark.parametrize(
+    "cls,expected",
+    [
+        (AtlCityCouncilSpider, 23),
+        (AtlCityCouncilFinSpider, 15),
+        (AtlCityCouncilUtilSpider, 15),
+        (AtlCityCouncilCOCSpider, 14),
+        (AtlCityCouncilCDHSpider, 17),
+        (AtlCityCouncilSafetySpider, 15),
+        (AtlCityCouncilTransportationSpider, 15),
+        (AtlCityCouncilZoningSpider, 16),
+    ],
+)
+def test_sub_spider(cls, expected):
+    spider = cls()
+    parsed_items = list(spider.parse(test_response))
+    assert len(parsed_items) == expected

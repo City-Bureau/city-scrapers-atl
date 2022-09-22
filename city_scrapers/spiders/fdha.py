87ae9a192ef8440eedee8f0ba064428b8c204067
Perry <rhetr@users.noreply.github.com>
2022-09-22 00:43:37 -0400

    fdha spider; still need location, links and tests

---
+++ city_scrapers/spiders/fdha.py
@@ -0,0 +1,20 @@
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.spiders import EventsCalendarSpider
+
+
+class FdhaSpider(EventsCalendarSpider):
+    name = "fdha"
+    agency = "Fulton-DeKalb Hospital Authority"
+    timezone = "America/New_York"
+    start_urls = ["https://thefdha.org/wp-json/tribe/events/v1/events"]
+    categories = {
+        BOARD: [
+            "public-notices",
+        ],
+    }
+
+    def _parse_location(self, item):
+        return {}
+
+    def _parse_links(self, item):
+        return {}
